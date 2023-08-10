936b170cb4ecce54815db38cecc9c21d4adbb6b7
chenglinfeng <1258781610@qq.com>
2023-08-10 10:55:03 +0800

    proj1 finished

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,38 +1,106 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"github.com/pkg/errors"
+	"path"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engine *engine_util.Engines
+	conf   *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dbPath := conf.DBPath
+
+	kvPath := path.Join(dbPath, "kv")
+	raftPath := path.Join(dbPath, "raft")
+
+	kvDB := engine_util.CreateDB(kvPath, false)
+	//project1没用到raft
+	raftDB := engine_util.CreateDB(raftPath, true)
+
+	//func CreateDB(path string, raft bool) *badger.DB
+	//func NewEngines(kvEngine, raftEngine *badger.DB, kvPath, raftPath string) *Engines
+	return &StandAloneStorage{
+		engine: engine_util.NewEngines(kvDB, raftDB, kvPath, raftPath),
+		conf:   conf,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+	if s.engine == nil || s.conf == nil {
+		return errors.New("Not NewStandAloneStorage")
+	}
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
-	return nil
+	return s.engine.Close()
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	txn := s.engine.Kv.NewTransaction(false)
+	return NewStandAloneStorageReader(txn), nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	var err error
+	for _, m := range batch {
+		key, val, cf := m.Key(), m.Value(), m.Cf()
+		if _, ok := m.Data.(storage.Put); ok {
+			err = engine_util.PutCF(s.engine.Kv, cf, key, val)
+		} else {
+			err = engine_util.DeleteCF(s.engine.Kv, cf, key)
+		}
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+//实现 type StorageReader interface 接口
+
+type StandAloneStorageReader struct {
+	KvTxn *badger.Txn
+}
+
+func NewStandAloneStorageReader(txn *badger.Txn) *StandAloneStorageReader {
+	return &StandAloneStorageReader{
+		KvTxn: txn,
+	}
+}
+
+func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(s.KvTxn, cf, key)
+	//StandAloneStorage是底层的一个系统,这里认为not found不是err
+	//将not found屏蔽掉
+	//上层不认为not found是err
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	}
+	return val, err
+}
+
+func (s *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	//func NewCFIterator(cf string, KvTxn *badger.Txn) *BadgerIterator
+	return engine_util.NewCFIterator(cf, s.KvTxn)
+}
+
+func (s *StandAloneStorageReader) Close() {
+	s.KvTxn.Discard()
 }
